5839f28037e17d264a6f6b2a52553f5b5e5c03ef
vote-vog <gaara5249@gmail.com>
2025-11-26 03:17:00 +0500

    fix: use native invisible yandex metrika integration

--- src/components/YandexMetrika.tsx
+++ src/components/YandexMetrika.tsx
@@ -1,19 +1,28 @@
 // src/components/YandexMetrika.tsx
-import { YMInitializer } from 'react-yandex-metrika';
+import { useEffect } from 'react';
 
-const YANDEX_COUNTER_ID = 105508703; // ⚡ Ваш реальный ID!
+const YANDEX_COUNTER_ID = 105508703;
 
-export const YandexMetrika = () => (
-  <YMInitializer 
-    accounts={[YANDEX_COUNTER_ID]} 
-    options={{
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true,
-      trackHash: true,
-      ecommerce: true
-    }}
-    version="2"
-  />
-);
+export const YandexMetrika = () => {
+  useEffect(() => {
+    // Нативная интеграция Яндекс.Метрики
+    if (typeof window !== 'undefined') {
+      (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(YANDEX_COUNTER_ID, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        ecommerce: true,
+        trackHash: true
+      });
+    }
+  }, []);
+
+  return null; // Полностью невидимый
+};
